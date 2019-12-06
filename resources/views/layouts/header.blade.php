bae27ed478eab45346460e91e7cfb8c8f0425d03
unique211 <vishal.rkcet@gmail.com>
2019-12-06 14:20:42 +0530

    Update header.blade.php
    add true as parameter in assets()

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,30 +17,30 @@
 
     {{--  CSS INCLUDE  --}}
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('/resources/sass/theme.css',true) }}" />
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('/resources/sass/custom.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('/resources/sass/custom.css',true) }}" />
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
     {{--  EOF CSS INCLUDE  --}}
-    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/style1.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/style1.css',true) }}">
 
 
     {{--  datatable CSS  --}}
-    <link rel="stylesheet" href="{{ URL::asset('resources/datatable/css/jquery.dataTables.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('resources/datatable/css/jquery.dataTables.css',true) }}">
     {{--  tost msg  --}}
-    <link href="{{ URL::asset('/resources/toastr/toastr.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('/resources/toastr/toastr.min.css',true) }}" rel="stylesheet">
     {{--  Sweetalert  --}}
-    <link href="{{ URL::asset('/resources/sweetalert/sweetalert.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('/resources/sweetalert/sweetalert.css',true) }}" rel="stylesheet">
 
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/clockpicker/0.0.7/bootstrap-clockpicker.min.css">
 
-    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/bootstrap-datepicker3.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/bootstrap-datetimepicker.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/bootstrap-datepicker3.min.css',true) }}">
+    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/bootstrap-datetimepicker.css',true) }}">
 
     {{--  <link href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet"
         type="text/css" />  --}}
-    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/multi_select/bootstrap-multiselect.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/multi_select/multiselect.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/multi_select/bootstrap-multiselect.css',true) }}">
+    <link rel="stylesheet" href="{{ URL::asset('/resources/sass/multi_select/multiselect.css',true) }}">
 
 
 </head>
