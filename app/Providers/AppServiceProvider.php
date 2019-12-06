2fafe428e98fe988be83a9d7c42e3e16ab270cca
unique211 <vishal.rkcet@gmail.com>
2019-12-06 17:20:30 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Routing\UrlGenerator;   //change by vishal
 
 class AppServiceProvider extends ServiceProvider
 {
