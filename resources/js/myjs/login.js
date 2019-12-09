5d8611aa11cd5aa1260c6e299612759f95db5842
unique211 <vishal.rkcet@gmail.com>
2019-12-09 12:20:32 +0530

    Update login.js

--- resources/js/myjs/login.js
+++ resources/js/myjs/login.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-   
+   $.ajaxSetup({
+        headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+        });
     /*---------login-----------------*/
     $(document).on("submit", "#loginform", function(e) {
         e.preventDefault();
