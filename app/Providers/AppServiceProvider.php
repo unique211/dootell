c85500d8bab5f5b9c35987d52f5a391238a04f6b
unique211 <vishal.rkcet@gmail.com>
2019-12-09 09:59:15 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-      if (env('APP_ENV') != 'local') {
-        URL::forceSchema('https');
+     if (env('APP_ENV') !== 'local') {
+        URL::forceScheme('https');
     }
     }
 }
